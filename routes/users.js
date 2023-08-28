332abc0c1596b8c3da4154d1bba5bbdef89745a2
Larisa Kindalova <120237097+LarisaKindalova@users.noreply.github.com>
2023-08-28 23:06:10 +1000

    Delete routes directory

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const userRouter = require('express').Router();
-const { getCurrentUser, updateUserInfo } = require('../controllers/users');
-const { validateUpdateUser } = require('../middlewares/validate');
-
-userRouter.get('/me', getCurrentUser);
-userRouter.patch('/me', validateUpdateUser, updateUserInfo);
-
-module.exports = userRouter;
